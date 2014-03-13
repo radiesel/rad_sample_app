012acf543d275fa86f0a8695b0ef380a056e35e5
Ric Diesel <ric.diesel@gmail.com>
2014-03-13 17:11:50 -0500

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,6 +16,7 @@ describe "Static pages" do
 
     it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
+ #  it { should_not have_selector 'title', text: '| Home' }
   end
 
   describe "Help page" do
@@ -50,12 +51,13 @@ describe "Static pages" do
     click_link "About"
     expect(page).to have_title(full_title('About Us'))
     click_link "Help"
-    expect(page).to # fill in
+    expect(page).to have_title(full_title('Help'))
     click_link "Contact"
-    expect(page).to # fill in
+    expect(page).to have_title(full_title('Contact'))
     click_link "Home"
     click_link "Sign up now!"
-    expect(page).to # fill in
+    expect(page).to have_title(full_title('Sign up'))
     click_link "sample app"
-    expect(page).to # fill in
+    expect(page).to have_title(full_title(''))
   end
+end
