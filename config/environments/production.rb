63aec25406a02f0c3d29d7969b2fc2cfbcd8e1b0
Ric Diesel <ric.diesel@gmail.com>
2014-04-08 15:54:09 -0500

    precompile css for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,16 @@ RadSampleApp::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # http://stackoverflow.com/questions/7464544/ruby-on-rails-rake-assetsprecompile-error
+  config.serve_static_assets = true 
+  # http://stackoverflow.com/questions/7464544/ruby-on-rails-rake-assetsprecompile-error
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = false 
 
   # Generate digests for assets URLs.
   config.assets.digest = true
